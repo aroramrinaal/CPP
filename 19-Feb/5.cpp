e2c14a4e9c9f7abfbe8bcb43670ff7647955786a
aroramrinaal <aroramrinaal@gmail.com>
2022-02-19 21:20:53 +0530

    C++

--- 19-Feb/5.cpp
+++ 19-Feb/5.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
@@ -7,9 +8,13 @@ int main(){
     cout<<decision1<<endl;
    
     bool decision2 = false;
-    cout<<decision2;
+    cout<<decision2<<endl;
 
 
-    double a = 25.123456;
-    cout<<a;
+    // double a = 25.123456;
+    // cout<<a<<endl;
+    string test_string;
+    test_string = "Mrinaal";
+    cout<<test_string;
+    return 0;
 }
