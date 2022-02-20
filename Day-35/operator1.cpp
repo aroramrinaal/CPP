d056a91cbb9b7670a21d6b2a7b555c065f37d880
aroramrinaal <aroramrinaal@gmail.com>
2022-02-20 13:19:46 +0530

    C++

---
+++ Day-35/operator1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   
+    int a = 10;
+    int b =20;
+    int c = 30;
+  
+  
+  
+    cout<<((a == b))<<endl;
+
+    cout<<((a > b) && (b>c));
+    /* code */
+    return 0;
+}
