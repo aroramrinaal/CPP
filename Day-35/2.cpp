ace134331c172b99e17d541948bb3a350d40bd59
aroramrinaal <aroramrinaal@gmail.com>
2022-02-20 11:48:38 +0530

    C++

--- Day-35/2.cpp
+++ Day-35/2.cpp
@@ -4,13 +4,16 @@ using namespace std;
 int main()
 {
     int sum, sub, prod;
+    double divv;
     int a = 20;
     int b = 30;
     sum = a + b;
     sub = b - a;
     prod  = a * b;
+    divv = a/b;
 
     cout<<sum<<endl;
     cout<<sub<<endl;
     cout<<prod<<endl;
+    cout<<divv<<endl;
 }
