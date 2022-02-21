de449277f1677dbafd5cd718cd00e817dc176372
aroramrinaal <aroramrinaal@gmail.com>
2022-02-21 17:19:58 +0530

    C++

---
+++ Day-36/4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /* code */
+    int n;
+    cout<<"Enter a number (N): "<<endl;
+    cin>>n;
+    if (n > 10)
+    {
+        /* code */
+        cout<<"Number is greater than 10."<<endl;
+    }
+    else{
+        cout<<"The number is lower than 10."<<endl;
+    }
+    
+
+    return 0;
+}
