020fe94e2885fd3374fe425b006b0977b5ebf4f9
aroramrinaal <aroramrinaal@gmail.com>
2022-02-21 14:13:24 +0530

    Cpp

--- Day-36/2.cpp
+++ Day-36/2.cpp
@@ -4,6 +4,13 @@ using namespace std;
 int main()
 {
     /* code */
-    
+    int i = 10;
+    if (i > 15)
+    {
+        /* code */
+        cout<<"10 is less than 15";
+
+    }
+    cout<<"I am Not in if";
     return 0;
 }
