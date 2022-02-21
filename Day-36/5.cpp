61bcb6b8ccda6d06685e5a9dfaef14654b14a240
aroramrinaal <aroramrinaal@gmail.com>
2022-02-21 17:47:56 +0530

    C++

---
+++ Day-36/5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /* code */
+    int i = 40;
+    if (i >30)
+    {
+        /* code */
+        cout<<"Greater than 30.";
+    }
+    
+    return 0;
+}
