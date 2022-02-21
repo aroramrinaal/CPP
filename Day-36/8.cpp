d603ffb7550c4dd5aeae47b894136fff617099d0
aroramrinaal <aroramrinaal@gmail.com>
2022-02-21 21:34:02 +0530

    C++

---
+++ Day-36/8.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /* code */
+    int i;
+    cout<<"Enter a Number:"<<endl;
+    cin>>i;
+    if (i%2==0)
+    {
+        /* code */
+        cout<<"Number is Even.";
+    }
+    else{
+        cout<<"Number is Odd.";
+    }
+    return 0;
+}
