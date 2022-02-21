aa5f147beb1f4701a36b3237d2755756e378bd61
aroramrinaal <aroramrinaal@gmail.com>
2022-02-21 21:47:30 +0530

    C++

---
+++ Day-36/9.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /* code */
+    int i;
+    cin>>i;
+    if (i == 10)
+    {
+        /* code */
+        cout<<"i is 10.";
+    }
+    else if(i == 15) {
+        cout<<"i is 15.";
+    }
+    else if (i == 20)
+    {
+        /* code */
+        cout<<"i is 20.";
+    }
+    else{
+        cout<<"i is not present.";
+    }
+    
+    
+    return 0;
+}
