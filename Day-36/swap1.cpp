c430aa39c5c8151251d81e34693a8f2a9180ff1d
aroramrinaal <aroramrinaal@gmail.com>
2022-02-21 09:17:22 +0530

    swap c++

---
+++ Day-36/swap1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    // Program for swapping values of two variables 
+
+    int a = 20;
+    int b = 10;
+
+    int temp = a;
+    a = b;
+    b = temp;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    return 0;
+}
