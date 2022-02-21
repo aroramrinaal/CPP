6d622febf4232fbdfdc2b3aa6b40deb51f50cfb1
aroramrinaal <aroramrinaal@gmail.com>
2022-02-21 16:12:32 +0530

    if statement in C++

---
+++ Day-36/3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /* code */
+    int i = 10;
+    if (i >= 10)
+    {
+        /* code */
+        cout<<"This Runs inside IF"<<endl;
+    }
+    cout<<"This runs outside IF.";
+    return 0;
+}
