b528de64c0475b603952b298d2006a4d8c793a92
aroramrinaal <aroramrinaal@gmail.com>
2022-02-21 21:06:42 +0530

    C++

---
+++ Day-36/7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /* code */
+    int i = 10;
+    if (i>10)
+    {
+        /* code */
+        cout<<"This does not run ";
+    }
+    cout<<"This is out of If Block";
+    
+    return 0;
+}
