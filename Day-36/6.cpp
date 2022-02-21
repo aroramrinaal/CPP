ae9d1211090f0f411ff8a2ba4eee294b00f5eb54
aroramrinaal <aroramrinaal@gmail.com>
2022-02-21 19:31:46 +0530

    C++

---
+++ Day-36/6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /* code */
+    int a , b , c;
+    cout<<"Enter A :"<<endl;
+    cin>>a;
+    cout<<"Enter b :"<<endl;
+    cin>>b;
+    cout<<"Enter C :"<<endl;
+    cin>>c;
+    if ((a>b) && (a>c))
+    {
+        /* code */
+        cout<<"A is greater than both b and c";
+    }
+    return 0;
+}
