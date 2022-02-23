fad0333eb891400c97108f36541ad74d62436ef0
aroramrinaal <aroramrinaal@gmail.com>
2022-02-23 17:30:33 +0530

    C++

---
+++ Day-38/9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Initialization expression
+    int i = 3;
+    do
+    {
+        // Statement of body of loop
+        cout << "Inside body of do-while loop" << endl;
+        // Update expression
+        i++;
+    } while (i < 1); // Update expression.
+}
