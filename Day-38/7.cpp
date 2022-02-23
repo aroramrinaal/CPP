dfac18e528017708c895e3faa16d71aee976fc2e
aroramrinaal <aroramrinaal@gmail.com>
2022-02-23 15:44:13 +0530

    C++

---
+++ Day-38/7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /* code */
+    int counter = 0;
+    while (counter < 3)
+    {
+        /* code */
+        cout<<"meow\n";
+        counter += 1;
+    }
+    
+    return 0;
+}
