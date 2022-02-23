015520238d0d8bab71f219b0a7757de6c3651630
aroramrinaal <aroramrinaal@gmail.com>
2022-02-23 11:04:17 +0530

    C++

---
+++ Day-38/2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /* code */
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        cout<<"HELLO"<<endl;
+    }
+    
+    return 0;
+}
+
