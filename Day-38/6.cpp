0c7cd8e6d49a12f0efbb386691515449cb646874
aroramrinaal <aroramrinaal@gmail.com>
2022-02-23 15:25:11 +0530

    C++

--- Day-38/6.cpp
+++ Day-38/6.cpp
@@ -3,6 +3,15 @@ using namespace std;
 int main()
 {
     /* code */
-    
+    int number , sum = 0;
+    cout<<"Enter a positive number :\n";
+    cin>>number;
+    while (number>0)
+    {
+        /* code */
+        sum = sum + (number%10);
+        number = number/10;
+    }
+    cout<<"Sum Of digits is:"<<sum;
     return 0;
 }
