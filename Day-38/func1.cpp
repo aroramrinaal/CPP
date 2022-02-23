ff726a18bd5024c9ffb3077bd958ecf5dbeec4a3
aroramrinaal <aroramrinaal@gmail.com>
2022-02-23 19:29:55 +0530

    function in C++

--- Day-38/func1.cpp
+++ Day-38/func1.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int max(int x , int y){
+    if (x>y)
+    {
+        return x;
+    }
+    else{
+        return y;
+    }
+    
+}
+
 int main()
 {
     /* code */
