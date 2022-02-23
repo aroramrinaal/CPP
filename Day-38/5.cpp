788e75e136823f77a9777ed350c5e725e9a5d19f
aroramrinaal <aroramrinaal@gmail.com>
2022-02-23 15:05:53 +0530

    C++

---
+++ Day-38/5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /* code */
+    int n, sum;
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        /* code */
+        sum = sum + i;
+
+    }
+    cout<<sum;
+    
+    return 0;
+}
