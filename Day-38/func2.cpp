45242f954159b1039898676a0b1f0ddc4484c863
aroramrinaal <aroramrinaal@gmail.com>
2022-02-23 19:44:03 +0530

    Functions In C++.

---
+++ Day-38/func2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int findSum(int a, int b) {
+    int sum = a + b;
+    return sum;
+}
+
+int main()
+{
+    /* code */
+    cout<<findSum(40,30)<<endl;
+    return 0;
+}
