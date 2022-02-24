7be7c5953e5bafca0559ead7dcac4fa8e4f42f18
aroramrinaal <aroramrinaal@gmail.com>
2022-02-24 09:08:12 +0530

    Day-39 of 100DaysOfC++

---
+++ Day-39/1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int findSum(int a , int b){
+    int sum = a + b ;
+    return sum;
+}
+
+int main()
+{
+    cout<<findSum(30,40)<<endl;
+    return 0;
+}
