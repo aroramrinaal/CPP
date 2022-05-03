43c37e72cbf0295dbac04a460ce811e392179c0e
aroramrinaal <aroramrinaal@gmail.com>
2022-05-03 15:06:36 +0530

    Inheritance in C++

--- 3-May/1.cpp
+++ 3-May/1.cpp
@@ -1,30 +1,3 @@
 // Inheritance in C++
 
-#include<iostream>
-using namespace std;
-
-class Vehicle
-{
-    public:
-    void fuelAmount(){
-        cout<<"What's the maximum fuel that can be added to this Vehicle."<<endl;
-    }
-    void capacity(){
-        cout<<"What's the maximum capacity of this vehicle?"<<endl;
-    }
-};
-
-
-class Car : public Vehicle
-{};
-
-class Bus : public Vehicle
-{};
-
-class Truck : public Vehicle
-{};
-
-int main(){
-    
-    return 0;
-}
+// Inheritance in C++
