68ad5668e331032dd8b74b2c861a918abd291519
aroramrinaal <aroramrinaal@gmail.com>
2022-05-03 15:47:23 +0530

    Inheritance in C++

--- 3-May/1.cpp
+++ 3-May/1.cpp
@@ -3,3 +3,27 @@
 // Inheritance in C++
 
 // Inheritance in C++
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    /* code */
+    Car obj1;
+    obj1.
+    return 0;
+}
+
+
+class Vehicle
+{
+    public:
+    void whatIsThis(){
+        cout<<"This is a Vehicle."<<endl;
+    }
+};
+
+
+class Car : public Vehicle
+{};
+
