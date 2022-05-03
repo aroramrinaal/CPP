7ad09a6e353b9357ca89a84b944d3c914e4d5a9b
aroramrinaal <aroramrinaal@gmail.com>
2022-05-03 15:08:16 +0530

    Inheritance in C++

--- 3-May/1.cpp
+++ 3-May/1.cpp
@@ -1,3 +1,30 @@
 // Inheritance in C++
 
 // Inheritance in C++
+
+
+#include<iostream>
+using namespace std;
+
+
+class Car
+{
+    public:
+    void fuel(){
+
+    }
+};
+
+
+class Bike
+{
+    public:
+    void fuel(){
+        
+    }
+};
+int main()
+{
+    /* code */
+    return 0;
+}
