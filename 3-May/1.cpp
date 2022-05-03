69a065c1f3d05f007466710458db7cf19de720bf
aroramrinaal <aroramrinaal@gmail.com>
2022-05-03 11:08:06 +0530

    C++

--- 3-May/1.cpp
+++ 3-May/1.cpp
@@ -2,32 +2,29 @@
 using namespace std;
 
 
-class Vehicle
+class Myclass
 {
     public:
-    string brand;
-    void honk(){
-        cout<<"Tuut, tuut! \n";
+    void myFunction(){
+        cout<<"Content Defined Under Parent Class";
     }
 };
 
-class Car : public Vehicle
+class Mychild : public Myclass
 {
-    public:
-    string model;
-    void printSpecialProperty(){
-        cout<<"4 Wheeler\n";
-    }
+    
 };
+
+
+class MyGrandChild : public Mychild
+{
+
+};
+
 
 int main()
 {
     /* code */
-    Car mycar;
-    mycar.honk();
-    mycar.brand = "Ford";
-    mycar.model = "Mustang";
-    cout<<"Brand of my car is: "<<mycar.brand<<"\n";
-    cout<<"Model of my car is: "<<mycar.model<<"\n";
+
     return 0;
 }
