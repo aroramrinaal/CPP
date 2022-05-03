dc9bcfa956d1e1287a53e805352d89902ad269f0
aroramrinaal <aroramrinaal@gmail.com>
2022-05-03 15:43:59 +0530

    Inheritance in C++

--- 3-May/1.cpp
+++ 3-May/1.cpp
@@ -2,26 +2,4 @@
 
 // Inheritance in C++
 
-
-#include<iostream>
-using namespace std;
-
-class A
-{
-    public:
-    int x;
-    protected:
-    int y;
-    private:
-    void printmyName(){
-        cout<<"Mrinaal"<<endl;
-    }
-};
-
-
-class B :public A
-{};
-
-int main(){
-;
-}
+// Inheritance in C++
