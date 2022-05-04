7e3f6ed45ef768531f882888e29aad101387e79d
aroramrinaal <aroramrinaal@gmail.com>
2022-05-04 21:20:35 +0530

    Inheritance in C++

--- 3-May/1.cpp
+++ 3-May/1.cpp
@@ -6,7 +6,21 @@ class Vehicle
 {
     private:
     int rc_no;
-    
+    public:
+    string company;
+    void getinfo(){
+        cout<<"Enter rcno."<<endl;
+        cin>>rc_no;
+    }
+    void displayinfo(){
+        cout<<"Registration Number is: "<<rc_no;
+    }
+};
+
+class Car : public Vehicle
+{
+    public:
+    string modelname;
 };
 
 int main()
