67554a1c388657c67c8e6a317b64ebf4d29ad6fc
aroramrinaal <aroramrinaal@gmail.com>
2022-05-04 21:37:19 +0530

    Inheritance

--- 3-May/1.cpp
+++ 3-May/1.cpp
@@ -8,6 +8,14 @@ class Baseclass
     void display(){
         cout<<"This display is inside the base class."<<endl;
     }
+    protected:
+    void hiddencontent(){
+        cout<<"This is hidden content"<<endl;
+    }
+    private:
+    void thisistopsecret(){
+        cout<<"Catch me if you can."<<endl;
+    }
 };
 
 class DerivedClass:public Baseclass
@@ -16,6 +24,9 @@ class DerivedClass:public Baseclass
     void show(){
         cout<<"This show is inside the second class which is derived from parent class" << endl;
     }
+    void checker(){
+        hiddencontent();
+    }
 };
 
 int main()
@@ -23,7 +34,6 @@ int main()
     /* code */
     Baseclass obj1;
     DerivedClass obj2;
-    obj2.display();
-    obj2.show();
+    obj2.checker();
     return 0;
 }
