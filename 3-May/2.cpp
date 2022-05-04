91838cde0638d372b964b3b5132dce53c716d284
aroramrinaal <aroramrinaal@gmail.com>
2022-05-04 21:50:19 +0530

    void myFuction(){         cout<<"Some content in Myclass"<<endl;     }

--- 3-May/2.cpp
+++ 3-May/2.cpp
@@ -1,14 +1,32 @@
 #include<iostream>
 using namespace std;
 
-class Person
+class MyClass
 {
     public:
-    
+    void myFuction(){
+        cout<<"Some content in Myclass"<<endl;
+    }
 };
 
-class Employee
-{};
 
-class Teacher : public Person,public Employee
-{};
+class MyOtherClass
+{
+    public:
+    void myOtherFuction(){
+        cout<<"Some content in MyOtherClass"<<endl;
+    }
+};
+
+class MyChildClass : public MyClass , public MyOtherClass
+{
+
+};
+
+int main()
+{
+    /* code */
+    MyChildClass obj1;
+    obj1.myFuction();
+    return 0;
+}
