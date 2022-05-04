defa7c2642fe7ba9e5c1d467754dfa7e90a4430e
aroramrinaal <aroramrinaal@gmail.com>
2022-05-04 21:03:36 +0530

    Inheritance in C++

--- 3-May/1.cpp
+++ 3-May/1.cpp
@@ -2,15 +2,30 @@
 using namespace std;
 
 class Person
-{};
+{
+    protected:
+    char name[50];
+    public:
+    void getdata(){
+        cout<<"Enter name:";
+        cin>>name;
+    }
+
+    void display(){
+
+    }
+};
 
 
 class Employee : public Person
-{};
+{
+
+};
 
 
 int main()
 {
     /* code */
+    
     return 0;
 }
