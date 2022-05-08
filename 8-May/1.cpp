a62b61bbcfbe3c95651bd264e0b110f755506ff1
aroramrinaal <aroramrinaal@gmail.com>
2022-05-08 17:27:53 +0530

    Object-Oriented Programming

--- 8-May/1.cpp
+++ 8-May/1.cpp
@@ -14,6 +14,7 @@ class Circle
     void display(){
         cout<<"Radius:"<<radius<<endl;
         cout<<"Color:"<<color<<endl;
+        cout<<"Area:"<<getArea()<<endl;
     }
     Circle(){
         radius = 7;
