c3bab31711ed578971a79bfd2963d1056137545c
aroramrinaal <aroramrinaal@gmail.com>
2022-05-08 14:33:53 +0530

    Object-Oriented Programming

--- 8-May/1.cpp
+++ 8-May/1.cpp
@@ -1,55 +1,13 @@
-#include<iostream>
-using namespace std;
-
 // Object-Oriented Programming
 
-class Student
-{
-    public:
-    string name;
-    double grade;
-    void getName(){
-        cout<<name<<endl;
-    }
-    void printGrade(){
-        cout<<grade;
-    }
-};
 
-class Circle
-{
-    public:
-    double radius;
-    string color;
-    void getRadius(){
-        cout<<radius;
-    }
-    double getArea(){
-        return radius*radius*(3.14);
-    }
-    void getColor();
-};
 
-void Circle :: getColor(){
-    cout<<color;
-}
+#include<iostream>
+using namespace std;
 
 int main()
 {
     /* code */
-    Circle c1;
-    c1.color = "red";
-    // c1.radius = 7;
-    // cout<<c1.getArea();
-    c1.getColor();
     return 0;
 }
-
-class Person
-{
-    public:
-    string name;
-    int age;
-};
-
 
