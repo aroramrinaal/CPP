2f9928eee0ab8c23b5457e4da4c6502e5f24e0f0
aroramrinaal <aroramrinaal@gmail.com>
2022-05-08 15:44:20 +0530

    Object-Oriented Programming

--- 8-May/1.cpp
+++ 8-May/1.cpp
@@ -3,28 +3,15 @@
 using namespace std;
 
 
-class StudentMarks
+class BaseClass
 {
     public:
-    void displayMarks();
-    int maths;
-    int science;
-    StudentMarks(int mark1,int mark2){
-        maths = mark1;
-        science =mark2;
-    }
+    
 };
-
-void StudentMarks ::displayMarks(){
-    cout<<"Maths:"<<maths<<endl;
-    cout<<"Science:"<<science<<endl;
-}
 
 int main()
 {
     /* code */
-    StudentMarks m(90,85);
-    m.displayMarks();
-    return 0;
+
 }
 
