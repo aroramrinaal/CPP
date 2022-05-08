43f972a1a36343676ffd896416bf3e037325f63f
aroramrinaal <aroramrinaal@gmail.com>
2022-05-08 12:43:15 +0530

    Object-Oriented Programming

--- 8-May/1.cpp
+++ 8-May/1.cpp
@@ -3,21 +3,4 @@ using namespace std;
 
 // Object-Oriented Programming
 
-class Student
-{
-   public:
-   string name;
-   int age;
-   string gender;
-   void printName(){
-       cout<<name;
-   }
-};
 
-int main()
-{
-    /* code */
-    Student obj1;
-    obj1.name="Mrinaal";
-    obj1.printName();
-}
