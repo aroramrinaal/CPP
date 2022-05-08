294dce0e1c67454cc078fbd6bdc4f6228c77cb41
aroramrinaal <aroramrinaal@gmail.com>
2022-05-08 20:52:40 +0530

    Object-Oriented Programming

--- 8-May/1.cpp
+++ 8-May/1.cpp
@@ -2,45 +2,8 @@
 
 #include<iostream>
 using namespace std;
-
-
-class Person
-{
-   protected:
-   char name[50];
-   public:
-   void getData(){
-       cout<<"Enter Name:"<<endl;
-       cin>>name;
-   }
-   void display(){
-       cout<<"Name:"<<name<<endl;
-   }
-   private:
-   string sex;
-};
-
-class Employee : private Person
-{
-    public:
-    void employeeName(){
-        cout<<name;
-    }
-};
-
-
-
-class Teacher : public Employee
-{
-
-};
-
 int main()
 {
     /* code */
-    Employee obj1;
-    obj1.getData();
-    // Employee obj2;
-    // obj2.employeeName();
     return 0;
 }
