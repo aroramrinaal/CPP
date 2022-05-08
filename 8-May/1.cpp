81e3637a81fc88ff93c3694fecfc953d5da1285c
aroramrinaal <aroramrinaal@gmail.com>
2022-05-08 17:14:12 +0530

    Object-Oriented Programming

--- 8-May/1.cpp
+++ 8-May/1.cpp
@@ -8,16 +8,22 @@ class Circle
 {
     public:
     double radius;
-    void getRadius(){
-        cout<<radius;
-    }
+    void getRadius();
+    double getArea();
 };
 
-void Circle :: get
+void Circle :: getRadius(){
+    cout<<radius;
+}
+double Circle ::getArea(){
+    return radius*radius*(3.14);
+}
 
 int main()
 {
     /* code */
-
+    Circle c1;
+    c1.radius = 7;
+    cout<<c1.getArea();
     return 0;
 }
