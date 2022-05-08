00065e0e1608a477475df6304cbc6c1945690e02
aroramrinaal <aroramrinaal@gmail.com>
2022-05-08 12:47:38 +0530

    Object-Oriented Programming

--- 8-May/1.cpp
+++ 8-May/1.cpp
@@ -3,4 +3,25 @@ using namespace std;
 
 // Object-Oriented Programming
 
+class Class1
+{
+    public:
+    string data1;
+    int data2;
+    void function1(){
+        cout<<"Let's Go Function1";
+    }
+    void function2(){
+        cout<<"Let's Go Function2";
+    }
+};
+
+
+int main()
+{
+    /* code */
+    Class1 obj1;
+    obj1.function2();
+    return 0;
+}
 
