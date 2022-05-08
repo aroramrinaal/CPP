2bd8f0316c4ae78df2a2037b99196cfd0e355044
aroramrinaal <aroramrinaal@gmail.com>
2022-05-08 19:13:52 +0530

    Object-Oriented Programming

--- 8-May/1.cpp
+++ 8-May/1.cpp
@@ -4,37 +4,9 @@
 using namespace std;
 
 
-class Circle
-{
-    public:
-    double radius;
-    string color;
-    void getRadius();
-    double getArea();
-    void display(){
-        cout<<"Radius:"<<radius<<endl;
-        cout<<"Color:"<<color<<endl;
-        cout<<"Area:"<<getArea()<<endl;
-    }
-    Circle(){
-        radius = 7;
-        color = "Red";
-    }
-};
-
-void Circle :: getRadius(){
-    cout<<radius;
-}
-double Circle ::getArea(){
-    return radius*radius*(3.14);
-}
-
 int main()
 {
     /* code */
-    Circle c1;
-        // c1.radius = 7;
-        // cout<<c1.getArea();
-    c1.display();
     return 0;
 }
+
