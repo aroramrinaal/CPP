99287e1e78ba34773db3260d61379eaec4548396
aroramrinaal <aroramrinaal@gmail.com>
2022-05-08 12:05:03 +0530

    Object-Oriented Programming

--- 8-May/1.cpp
+++ 8-May/1.cpp
@@ -2,3 +2,17 @@
 using namespace std;
 
 // Object-Oriented Programming
+
+class Student
+{
+   string name;
+   int age;
+   bool pass_or_fail;
+};
+
+int main()
+{
+    /* code */
+    
+    return 0;
+}
