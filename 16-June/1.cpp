16eabb48f7c0b37b896e3ed3a52550a9f029d165
aroramrinaal <aroramrinaal@gmail.com>
2022-06-16 20:57:12 +0530

    CPP

--- 16-June/1.cpp
+++ 16-June/1.cpp
@@ -1,14 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[5],i;
-    cout<<"Size of this integer in this compiler is "<<sizeof(int)<<endl;
-    for (i=0;i<5;i++)
-    {
-        /* code */
-        cout<<"Address arr["<<i<<"] is "<<&arr[i]<<endl;
-        
-    }
-    
+
     return 0;
 }
